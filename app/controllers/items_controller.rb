a0d01502b08098b21c9cd477eac4cd9f0309327a
IshigeU <theishigedeluxe@gmail.com>
2021-02-11 19:04:51 +0900

    itemモデルとitemsテーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,19 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :description_id, :condition_id, :delivary_cost_id, :prefecture_id, :delivary_day_id, :items_category_id, :user)
   end
 end
