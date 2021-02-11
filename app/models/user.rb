5833589267ae6bad363e72fe1857b9e071d6f977
IshigeU <theishigedeluxe@gmail.com>
2021-02-11 14:43:04 +0900

    Deviseでログイン機能実装/RSpecにてテスト③

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,13 @@ class User < ApplicationRecord
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
     #sei,meiは漢字、ひらがな、カタカナのみ
     with_options format: {with: /\A[ぁ-んァ-ン一-龥々]/} do
-    validates :sei
-    validates :mei
+      validates :sei
+      validates :mei
     end
     #kana_sei,kana_meiはカタカナのみ
     with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-    validates :kana_sei
-    validates :kana_mei
+      validates :kana_sei
+      validates :kana_mei
     end
   end
 end
