e3c82d16d101d0ad512c36e5c7a6cb6c4db6da2a
IshigeU <theishigedeluxe@gmail.com>
2021-02-11 19:30:43 +0900

    Active Storage導入とマイグレート

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :description
+    validates :condition_id
+    validates :delivary_cost_id
+    validates :prefecture_id
+    validates :delivary_day_id
+    validates :items_category_id
+    validates :user
+  end
 end
