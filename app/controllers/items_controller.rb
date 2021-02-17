c870ce2e4541b2424ac32c9c932a6f925d20ee69
IshigeU <theishigedeluxe@gmail.com>
2021-02-17 22:24:27 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_unknown_user, only: [:edit, :update]
 
   def index
@@ -34,11 +34,10 @@ class ItemsController < ApplicationController
     end
   end
   
-  #def destroy
-    #@item = Item.find(prams[:id])
-    #@item.destroy
-    #redirect_to root_path
-  #end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 
