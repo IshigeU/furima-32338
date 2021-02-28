fd23c8ad3c5a07aaaca48f65b2080c5034c967ff
IshigeU <theishigedeluxe@gmail.com>
2021-02-28 15:14:43 +0900

    最終確認修正①

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -7,7 +7,7 @@ class OrderForm
 
     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
-    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
 
   def save
